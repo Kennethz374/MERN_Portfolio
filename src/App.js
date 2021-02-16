c7e320073758c3debf486ff66fc78322c6f0af58
kennethzhang <kennethzhang374@gmail.com>
2021-02-15 23:47:25 -0800

    setup all routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Home from "../src/pages/Home";
+import Ref from "../src/pages/Ref";
+import Project from "../src/pages/Project";
+import Error from "../src/pages/Error";
 
 function App() {
 	return (
 		<Router>
 			<Switch>
 				<Home exact path="/" />
+				<Project exact path="/project" />
+				<Ref exact path="/reference" />
+				<Error path="*" />
 			</Switch>
 		</Router>
 	);
---
+++ src/pages/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Error() {
+	return <div>No such page back to home</div>;
+}
+
+export default Error;
