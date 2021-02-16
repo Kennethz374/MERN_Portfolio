589f88f46d38fd9c91814047923e16ac82b795aa
kennethzhang <kennethzhang374@gmail.com>
2021-02-16 13:14:08 -0800

    menubtn complete, start sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
+import logo from "../images/Kenz.png";
 
 function Navbar() {
 	return (
 		<nav className="nav">
-			<img src="../images/Kenz.png" alt="logo" className="logo" />
+			<img src={logo} alt="logo" className="logo" />
 			<div className="menu-btn">
 				<FaBars />
 			</div>
