926c78f1b9b5985687a92d835d8fb2d9eb16d84b
kennethzhang <kennethzhang374@gmail.com>
2021-02-16 23:29:03 -0800

    changing format

--- src/pages/Ref.js
+++ src/pages/Ref.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGripVertical } from "react-icons/fa";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 function Ref() {
@@ -10,29 +11,33 @@ function Ref() {
 				<p> What they say about me</p>
 			</div>
 
-			<div className="icon-container">
-				<button className="ref-icon">
-					<IoIosArrowBack />
-				</button>
-				<button className="ref-icon">
-					<IoIosArrowForward />
-				</button>
-			</div>
+			{/* <div className="reference-container">
+				<div className="icon-left">
+					<button className="ref-icon">
+						<IoIosArrowBack className="icon" />
+					</button>
+				</div>
+				<div className="icon-right">
+					<button className="ref-icon">
+						<IoIosArrowForward className="icon" />
+					</button>
+				</div>
 
-			<div className="reference-container">
-				<img src="https://i.ibb.co/HYmr2wB/SFU.png" alt="WY" />
-				<h1 className="ref-name">William Yu</h1>
-				<h2 className="ref-title">Production Manager</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque amet
-					nesciunt eum sunt nam magnam nemo enim. Laboriosam deserunt sunt
-					accusantium atque. Doloremque vero necessitatibus nulla cum, assumenda
-					recusandae animi! Vitae doloribus, sed sequi eum id dolore saepe
-					aspernatur illum repudiandae autem eaque quaerat voluptatibus mollitia
-					repellendus voluptatum enim necessitatibus voluptates a eveniet
-					veniam?
-				</p>
-			</div>
+				<div className="ref-card">
+					<img src="https://i.ibb.co/HYmr2wB/SFU.png" alt="WY" />
+					<h1 className="ref-name">William Yu</h1>
+					<h2 className="ref-title">Production Manager</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque amet
+						nesciunt eum sunt nam magnam nemo enim. Laboriosam deserunt sunt
+						accusantium atque. Doloremque vero necessitatibus nulla cum,
+						assumenda recusandae animi! Vitae doloribus, sed sequi eum id dolore
+						saepe aspernatur illum repudiandae autem eaque quaerat voluptatibus
+						mollitia repellendus voluptatum enim necessitatibus voluptates a
+						eveniet veniam?
+					</p>
+				</div>
+			</div> */}
 		</section>
 	);
 }
