698d0a6f0ee05ac536dd66c39b4b57a9f4b9b134
kennethzhang <kennethzhang374@gmail.com>
2021-02-19 23:10:44 -0800

    entering real data

--- src/components/CvModal.js
+++ src/components/CvModal.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 import { useGlobalContext } from "../context";
+import CV from "../images/CV.png";
+
 function CvModal() {
 	const { closeCvModal, isCvModalOpen } = useGlobalContext();
 	return (
 		<div className={`modal-overlay ${isCvModalOpen ? "show-modal" : null}`}>
 			<div className="modal-container">
-				<img src="https://i.ibb.co/HYmr2wB/SFU.png" alt="CV" />
+				<img src={CV} alt="CV" />
 				<button className="close-modal-btn" onClick={closeCvModal}>
 					<FaTimes />
 				</button>
