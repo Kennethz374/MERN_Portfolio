78ed15df399648ca0c37ac75ebf6d79700c39abb
kennethzhang <kennethzhang374@gmail.com>
2021-02-24 15:07:54 -0800

    update projects

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,5 +1,7 @@
 import React from "react";
 import covid from "../images/covid.png";
+import PhotoGallery from "../images/PhotoGallery.png";
+import ShoppingCart from "../images/ShoppingCart.png";
 
 function Project() {
 	return (
@@ -12,46 +14,40 @@ function Project() {
 			<div className="projects">
 				<div className="project-container">
 					<div className="project-title">
-						<h2>Covid Tracker</h2>
+						<h2>Photo Gallery</h2>
 					</div>
 
 					<div className="project-img">
-						<img src={covid} alt="" />
+						<img src={PhotoGallery} alt="" />
 					</div>
 
 					<div className="project-desc">
 						<div className="project-nav">
-							<a href="">Code</a>
-							<a href="">Site</a>
+							<a href="https://github.com/Kennethz374/PhotoGallery">Code</a>
+							<a href="https://kennethz374.github.io/PhotoGallery/">Site</a>
 						</div>
 						<div className="project-content">
-							<p>
-								A simple application tracking down numbers of covid cases
-								everyday
-							</p>
+							<p>PhotoGallery with infinite scroll fetching with React</p>
 						</div>
 					</div>
 				</div>
 
 				<div className="project-container">
 					<div className="project-title">
-						<h2>Covid Tracker</h2>
+						<h2>E-commerce Cart</h2>
 					</div>
 
 					<div className="project-img">
-						<img src={covid} alt="" />
+						<img src={ShoppingCart} alt="" />
 					</div>
 
 					<div className="project-desc">
 						<div className="project-nav">
-							<a href="">Code</a>
-							<a href="">Site</a>
+							<a href="https://github.com/Kennethz374/Cart">Code</a>
+							<a href="https://kennethz374.github.io/Cart/">Site</a>
 						</div>
 						<div className="project-content">
-							<p>
-								A simple application tracking down numbers of covid cases
-								everyday
-							</p>
+							<p>A refresher project about useReducer in React</p>
 						</div>
 					</div>
 				</div>
